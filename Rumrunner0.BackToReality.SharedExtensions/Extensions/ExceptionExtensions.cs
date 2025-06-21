47a5f8c88bdfa7fd9a7f2de31a380d84903b24ff
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2025-06-21 22:47:28 +0200

    Deleted HumanizeName exception extension, deleted Humanizer dependency

--- Rumrunner0.BackToReality.SharedExtensions/Extensions/ExceptionExtensions.cs
+++ Rumrunner0.BackToReality.SharedExtensions/Extensions/ExceptionExtensions.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Text;
-using Humanizer;
 
 namespace Rumrunner0.BackToReality.SharedExtensions.Extensions;
 
@@ -33,27 +32,5 @@ public static class ExceptionExtensions
 		}
 
 		return builder.ToString();
-	}
-
-	/// <summary>
-	/// Humanizes the name of a <paramref name="source" /> exception.
-	/// </summary>
-	/// <param name="source">The exception.</param>
-	/// <param name="includeEnding">The flag that indicates whether to include the "Exception" word.</param>
-	/// <returns>A humanized name.</returns>
-	public static string HumanizeName(this Exception source, bool includeEnding = false)
-	{
-		ArgumentNullException.ThrowIfNull(source);
-
-		var name = source.GetType().Name.Humanize();
-
-		if (!includeEnding)
-		{
-			const string EXCEPTION_WORD = "exception";
-			var i = name.LastIndexOf(EXCEPTION_WORD, StringComparison.OrdinalIgnoreCase);
-			if (i > 0 && i + EXCEPTION_WORD.Length == name.Length) name = name.Remove(startIndex: i);
-		}
-
-		return name.Trim();
 	}
 }
