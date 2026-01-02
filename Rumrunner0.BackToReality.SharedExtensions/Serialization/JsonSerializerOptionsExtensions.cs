fd066dd1e75633b58eebf46cc331dc359ab680cc
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2026-01-02 17:20:46 +0100

    Release 0.8.6
    * Added JsonSerializerOptionsExtensions with BetterWeb setup

---
+++ Rumrunner0.BackToReality.SharedExtensions/Serialization/JsonSerializerOptionsExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Rumrunner0.BackToReality.SharedExtensions.Serialization;
+
+/// <summary>Extensions for <see cref="JsonSerializerOptions" />.</summary>
+public static class JsonSerializerOptionsExtensions
+{
+	private static JsonSerializerOptions? _betterWeb;
+
+	/// <summary>Creates a new <see cref="JsonSerializerOptions" /> preconfigured with the default settings.</summary>
+	/// <returns>A new <see cref="JsonSerializerOptions" />.</returns>
+	public static JsonSerializerOptions BetterWeb
+	{
+		get
+		{
+			if (_betterWeb is { } cached) return cached;
+			_betterWeb = new JsonSerializerOptions().ConfigureBetterWeb();
+			_betterWeb.MakeReadOnly();
+			return _betterWeb;
+		}
+	}
+
+	/// <summary>Applies the default behavior.</summary>
+	/// <param name="options">The options.</param>
+	/// <returns>The same <paramref name="options" /> with applied defaults.</returns>
+	public static JsonSerializerOptions ConfigureBetterWeb(this JsonSerializerOptions options)
+	{
+		options.PropertyNameCaseInsensitive = true;
+		options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+		options.NumberHandling = JsonNumberHandling.Strict;
+
+		options.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+		options.IgnoreReadOnlyProperties = false;
+
+		options.WriteIndented = true;
+		options.IndentCharacter = '\t';
+		options.IndentSize = 1;
+
+		options.NewLine = "\n";
+		options.AllowTrailingCommas = false;
+
+		// If we ever need to extend the symbol range, here's the solution — but future us should perform extra research.
+		// Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic);
+
+		return options;
+	}
+}
